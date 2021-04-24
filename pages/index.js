bd073b06f5cadf0d5c94b193f57c9506aba5d7b4
Luis David Garcia Lunar <ldgl1215@gmail.com>
2021-04-24 19:33:02 -0400

    adding tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Barrakuda Sushi - Home</title>
         <link rel="icon" href="/sushi.svg" />
       </Head>
 
-      <main className={styles.main}>
-        <h1>Aqui aqui aqui</h1>
+      <main>
+        
       </main>
     </div>
   )
